6bcccf792cd1c8641c276a7a073165c1c58374ff
임동준 <armada55@naver.com>
2019-07-18 10:08:49 +0000

    update '__init__.py'

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -13,13 +13,13 @@ def helloworld():
 
 @app.route("/data",methods=['POST'])
 def receive():
-    #req_json =  {request.form['store_name'],request.form['gps_location']} #request.get_json()
+    req_json =  {request.form['store_name'],request.form['gps_lat'],request.form['gps_lon']} #request.get_json()
     #print(req_json)
     
-    store_info = cb.combine_info(request.form['store_name'],request.form['gps_location'])
-    datas = sd.convert_json(store_info)
+    #store_info = cb.combine_info(request.form['store_name'],request.form['gps_location'])
+    #datas = sd.convert_json(store_info)
 
     #store = Store("starbucks","시흥시 대야동","ICON","4.5","맛있어요","cafe")
     #datas = sd.convert_json(store)
-    return datas
+    return req_json
 
